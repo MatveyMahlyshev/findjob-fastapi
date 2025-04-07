a78b7c42540d5c53e39abddca9b49d9d9337c88b
MatveyMahlyshev <matthewfootballer11@gmail.com>
2025-04-07 16:57:52 +0300

    first endpoint

---
+++ backend/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return {
+        "message": "index",
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", reload=True)

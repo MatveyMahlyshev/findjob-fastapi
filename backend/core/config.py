ed33a6296e648bfb8868d51881b571e078832981
MatveyMahlyshev <matthewfootballer11@gmail.com>
2025-05-06 16:51:39 +0300

    authjwt settings

--- backend/core/config.py
+++ backend/core/config.py
@@ -8,13 +8,23 @@ BASE_DIR = Path(
 
 DB_PATH = BASE_DIR / "db.sqlite3"
 
+
+class AuthJWT(BaseModel):
+    private_key: Path = BASE_DIR / "certs" / "jwt-private.pem"
+    public_key: Path = BASE_DIR / "certs" / "jwt-public.pem"
+    algorithm: str = "RS256"
+    access_token_expire_minutes: int = 15
+    refresh_token_expire_days: int = 50
+
+
 class DBSettings(BaseModel):
-    url: str = f"sqlite+aiosqlite:///{DB_PATH}" 
+    url: str = f"sqlite+aiosqlite:///{DB_PATH}"
     echo: bool = True
 
 
 class Settings(BaseSettings):
     db: DBSettings = DBSettings()
     api_v1_prefix: str = "/api/v1"
+
 
 settings = Settings()

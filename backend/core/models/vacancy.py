31bdc0f313c8d79331d2a3bb7976e0cce95402eb
MatveyMahlyshev <matthewfootballer11@gmail.com>
2025-05-11 01:52:35 +0300

    added relation and field hr

--- backend/core/models/vacancy.py
+++ backend/core/models/vacancy.py
@@ -1,18 +1,28 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import Text
+from sqlalchemy import Text, ForeignKey
 from typing import TYPE_CHECKING
 
 from .base import Base
+from .user import User
+from .mixins import UserRelationMixin
 
 if TYPE_CHECKING:
     from .vacancy_skill_association import VacancySkillAssociation
 
 
 class Vacancy(Base):
+
     title: Mapped[str] = mapped_column(nullable=False, index=True)
     company: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(Text(), nullable=False)
+    hr_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"), unique=False, nullable=False
+    )
 
     vacancy_skills: Mapped[list["VacancySkillAssociation"]] = relationship(
         back_populates="vacancy"
+    )
+    hr: Mapped["User"] = relationship(
+        "User",
+        back_populates="vacancies",
     )

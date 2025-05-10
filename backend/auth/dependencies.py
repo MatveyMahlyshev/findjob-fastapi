422ddcd4818067159a6e81d419cac1f39bda90be
MatveyMahlyshev <matthewfootballer11@gmail.com>
2025-05-11 01:50:52 +0300

    auth dependencies

--- backend/auth/dependencies.py
+++ backend/auth/dependencies.py
@@ -1,30 +1,12 @@
 from fastapi.security import OAuth2PasswordBearer, HTTPBearer
-from fastapi import HTTPException, status, Depends
+from fastapi import Depends
 from jwt.exceptions import InvalidTokenError
 from datetime import timedelta
 
 
 from .utils import encode_jwt, decode_jwt
 from core.config import settings
-
-
-class UnauthorizedExceptions:
-    INVALID_LOGIN_DATA = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid email or password.",
-    )
-    INVALID_TOKEN = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail=f"Invalid token.",
-    )
-    ERROR_TOKEN_TYPE = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Error type of token.",
-    )
-    NO_EMAIL = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Token does not contain email",
-    )
+import exceptions
 
 
 class TokenTypeFields:
@@ -36,13 +18,14 @@ class TokenTypeFields:
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login/")
 http_bearer = HTTPBearer(auto_error=False)
 
+
 def get_current_token_payload(
     token: str = Depends(oauth2_scheme),
 ) -> dict:
     try:
         payload = decode_jwt(token=token)
     except InvalidTokenError:
-        raise UnauthorizedExceptions.INVALID_TOKEN
+        raise exceptions.UnauthorizedExceptions.INVALID_TOKEN
     return payload
 
 
@@ -64,4 +47,4 @@ def create_token(
 def validate_token_type(payload: dict, token_type: str) -> bool:
     if payload.get(TokenTypeFields.TOKEN_TYPE_FIELD) == token_type:
         return True
-    raise UnauthorizedExceptions.ERROR_TOKEN_TYPE
+    raise exceptions.UnauthorizedExceptions.ERROR_TOKEN_TYPE

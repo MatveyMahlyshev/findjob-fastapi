a1c0045d8ee474b6e59b025ac23fb51acce7c897
MatveyMahlyshev <matthewfootballer11@gmail.com>
2025-05-16 00:17:10 +0300

    cors

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 import uvicorn
 from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from api_v1 import router as api_v1_router
 from core.config import settings
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -16,6 +19,13 @@ app.include_router(
     prefix=settings.api_v1_prefix,
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],  # или ["*"] для всех
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
